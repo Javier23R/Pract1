0d60b4d2221713a21122c7b48380e0e1be2f5fbb
Javier Esteban Rodriguez <jrodriguez72@udi.edu.co>
2021-09-02 21:20:14 -0500

    Actualizacion Table1.0

--- Sqlprac1/Sqlprac1/ALUMNO.sql
+++ Sqlprac1/Sqlprac1/ALUMNO.sql
@@ -1,4 +1,7 @@
 ﻿CREATE TABLE [dbo].[ALUMNO]
 (
-	[Id] INT NOT NULL PRIMARY KEY
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Nombre] NVARCHAR(50) NOT NULL, 
+    [Ciudad] NVARCHAR(50) NOT NULL, 
+    [Edad] INT NOT NULL
 )
