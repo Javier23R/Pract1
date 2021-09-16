4e591df94b4cd03502cc1eaac6229458635b9db2
Javier Esteban Rodriguez <jrodriguez72@udi.edu.co>
2021-09-16 15:34:21 -0500

    From1.0

--- FormPract1/FormPract1/Form1.cs
+++ FormPract1/FormPract1/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,20 @@ namespace FormPract1
 {
     public partial class Form1 : Form
     {
+        public SqlConnection con = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DBPract1;Integrated Security=True");
+        SqlCommand cmd;
+        SqlDataReader dr;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            con.Open();
+            MessageBox.Show("Conexion creada con exito!");
+            con.Close();
+ 
         }
     }
 }
