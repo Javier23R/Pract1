7307975e7054b0f7084518748ade2f3ef2219f53
Javier R <jrodriguez72@udi.edu.co>
2021-11-11 21:35:51 -0500

    12

--- BDMercamax/VerProducto.sql
+++ BDMercamax/VerProducto.sql
@@ -1,6 +1,5 @@
-﻿CREATE PROCEDURE [dbo].[VerProducto]
-	@param1 int = 0,
-	@param2 int
+﻿CREATE VIEW [dbo].[VerProducto]
+
 AS
-	SELECT @param1, @param2
-RETURN 0
+	SELECT nombre_producto,precio FROM Producto
+
